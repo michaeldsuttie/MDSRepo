9c061c1384f28e2fc68608e046f768d048637e5e
msuttie <msuttie@dstcontrols.com>
2015-10-01 08:57:27 -0700

    Initial Commit

---
+++ wip/Fitbit_to_PI/Fitbit_to_PI/FitbitDataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitbit_to_PI
+{
+    public class FitbitDataService
+    {
+        public FitbitDataService()
+        {
+            //GetData();
+        }
+        static public void main()
+        {
+            string from = "2015-04-25";
+            string through = "2015-04-26";
+            string url = "https://toggl.com/reports/api/v2/details?workspace_id=669485&since=" + from + "&until=" + through + "&user_agent=api_test&rounding=on";
+
+            string ApiToken = "a2e7f93070afe8475626141477fec42b";
+            string userpass = ApiToken + ":api_token";
+            string userpassB64 = Convert.ToBase64String(Encoding.Default.GetBytes(userpass.Trim()));
+            string authHeader = "Basic " + userpassB64;
+        }
+    }
+
+
+}
---
+++ wip/Fitbit_to_PI/Fitbit_to_PI/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitbit_to_PI
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            //FitbitDataService.GetData();
+        }
+    }
+}
