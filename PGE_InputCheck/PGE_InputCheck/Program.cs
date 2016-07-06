0e08e2ee04d3c529b81a4d2cc368a2bcbb2307d6
Mike Suttie <MSuttie@DSTControls.local>
2016-07-06 13:37:31 -0700

    PGE_InputCheck v1.2 Created

--- PGE_InputCheck/PGE_InputCheck/Program.cs
+++ PGE_InputCheck/PGE_InputCheck/Program.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
 
 #if DEBUG
 
