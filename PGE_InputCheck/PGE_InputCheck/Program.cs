a38ed792144775b5ff8eb26b350717961753d357
Mike Suttie <MSuttie@DSTControls.local>
2016-07-08 14:05:09 -0700

    no message

--- PGE_InputCheck/PGE_InputCheck/Program.cs
+++ PGE_InputCheck/PGE_InputCheck/Program.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyVersion("1.3.*")]
 
 #if DEBUG
 
